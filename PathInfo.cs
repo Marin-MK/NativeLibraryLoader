f656ad44f52035cb5cfb29ed63810c4193b961eb
Marijn Herrebout <m.k.herrebout@student.tudelft.nl>
2022-05-31 23:18:42 +0200

    Initial commit

---
+++ PathInfo.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NativeLibraryLoader;
+
+public class PathInfo
+{
+    private Dictionary<Platform, PathPlatformInfo> Paths = new Dictionary<Platform, PathPlatformInfo>();
+
+    private PathInfo()
+    {
+
+    }
+
+    private void AddPlatform(PathPlatformInfo Info)
+    {
+        if (Paths.ContainsKey(Info.Platform)) Paths.Remove(Info.Platform);
+        Paths.Add(Info.Platform, Info);
+    }
+
+    public static PathInfo Create(params PathPlatformInfo[] Platforms)
+    {
+        PathInfo info = new PathInfo();
+        for (int i = 0; i < Platforms.Length; i++)
+        {
+            info.AddPlatform(Platforms[i]);
+        }
+        return info;
+    }
+
+    public PathPlatformInfo GetPlatform(Platform Platform)
+    {
+        if (!Paths.ContainsKey(Platform)) throw new PlatformNotSupportedException();
+        return Paths[Platform];
+    }
+}
+
+public class PathPlatformInfo
+{
+    public Platform Platform;
+    public Dictionary<string, string> Paths;
+
+    public PathPlatformInfo(Platform Platform)
+    {
+        this.Platform = Platform;
+        this.Paths = new Dictionary<string, string>();
+    }
+
+    public void AddPath(string Key, string Value)
+    {
+        if (Paths.ContainsKey(Key)) Paths.Remove(Key);
+        this.Paths.Add(Key, Value);
+    }
+
+    public bool Has(string Key)
+    {
+        return Paths.ContainsKey(Key);
+    }
+
+    public string Get(string Key)
+    {
+        if (!Has(Key)) throw new MissingPathException(Key);
+        return Paths[Key];
+    }
+
+    public class MissingPathException : Exception
+    {
+        public MissingPathException(string Path) : base($"No path entry for key '{Path}'") { }
+    }
+
+}
